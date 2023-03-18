57c6e7f90ca3fd20fb23384f911e88a93e27ce88
ariellefeher <117110135+ariellefeher@users.noreply.github.com>
2023-03-18 12:30:54 +0200

    Working on Existing Reservations: Changes in LoginScreen, HomePage, ExistingReservations, app.js. Created Existing Fetcher Class

--- Client/app/src/main/java/com/example/milab_study_zone/ExistingReservations.java
+++ Client/app/src/main/java/com/example/milab_study_zone/ExistingReservations.java
@@ -1,46 +1,61 @@
 package com.example.milab_study_zone;
 
+import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.View;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.example.milab_study_zone.databinding.ActivityExistingReservationsBinding;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ExistingReservations extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-private ActivityExistingReservationsBinding binding;
+    private Button getResButton;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_reservations);
 
-     binding = ActivityExistingReservationsBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+        password = intent.getStringExtra("password");
 
-        setSupportActionBar(binding.toolbar);
+//       RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+//       recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_existing_reservations);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        getResButton = findViewById(R.id.existResButton);
+        getResButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                fetchReservations(v);
             }
         });
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_existing_reservations);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void fetchReservations(final View v) {
+
+        final ExistingReservationFetcher fetcher = new ExistingReservationFetcher(v.getContext());
+        fetcher.dispatchRequest(username, password, new ExistingReservationFetcher.ExisResResponseListener() {
+
+            public void onResponse(ExistingReservationFetcher.ExisResResponse response) {
+
+                if (response.isError) {
+                    Toast.makeText(v.getContext(), "Error Fetching User Reservations", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //if successful authentication
+
+            }
+
+        });
+
     }
 }
